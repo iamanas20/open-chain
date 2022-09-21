dfaa3c9d88fe70f5caeb8a4610b412b51b4f96a3
iamanas20 <anaslatique@gmail.com>
2022-09-21 22:33:36 +0200

    countdown works on the NFT

--- components/NftShowcase.tsx
+++ components/NftShowcase.tsx
@@ -1,8 +1,36 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/NftShowcase.module.scss';
 
 export function NftShowcase() {
+  const [timeLeft, setTimeLeft] = useState('');
+  useEffect(
+    () => {
+      const remainingTime = new Date;
+      remainingTime.setDate(remainingTime.getDate() + 1);
+
+      setInterval(
+        () => {
+          const now = new Date;
+          var seconds = Math.floor((remainingTime.getTime() - now.getTime()) / 1000);
+          var minutes = Math.floor(seconds / 60);
+          var hours = Math.floor(minutes / 60);
+          var days = Math.floor(hours / 24);
+
+          hours = hours - (days * 24);
+          minutes = minutes - (days * 24 * 60) - (hours * 60);
+          seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+
+          setTimeLeft(`${addZeros(hours, 2)}h : ${addZeros(minutes, 2)}m : ${addZeros(seconds, 2)}s`);
+        }, 1000
+      )
+    }, []
+  );
+
+  function addZeros(num: number, totalLength: number) {
+    return String(num).padStart(totalLength, '0');
+  }
+
   return (
     <div className={styles.nft}>
       <div className={styles.nftCover}>
@@ -23,7 +51,7 @@ export function NftShowcase() {
         <div className={styles.chainData}>
           <div className={styles.timeLeft}>
             <h5 className={styles.time}>
-              20h : 35m : 02s
+              {timeLeft}
             </h5>
             <p className={styles.timeText}>
               Remaining time
